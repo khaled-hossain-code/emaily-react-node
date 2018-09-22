17403a2e1355e8d8e1ece9ccee9695be369066b0
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-09-22 19:45:24 +0600

    Design header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,7 +9,7 @@ const Landing = () => <h2>Landing Component</h2>
 
 function App () {
   return (
-    <div>
+    <div className="container">
       <BrowserRouter>
         <div>
           <Header />
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 class Header extends Component {
   render(){
     return (
-      <div>
-        header
-      </div>
+      <nav>
+        <div className="nav-wrappere">
+          <a href="#" className="brand-logo">Emaily</a>
+          <ul className="right">
+            <li><a href="/auth/google">Login with Google</a></li>
+          </ul>
+        </div>
+      </nav>
     )
   }
-
 };
 
 export default Header;

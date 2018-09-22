b9b89f12c9bdb8a6178285881d5eba86e40b243b
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-09-22 20:41:10 +0600

    Add redux thunk for asynchronous actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,5 +2,8 @@ import { FETCH_USER } from './type';
 import axios from 'axios';
 
 const fetch_user = () => {
-  axios.get('/api/current_user');
+  return function(dispatch) {
+    axios.get('/api/current_user')
+    .then(user => dispatch({type: FETCH_USER, payload: user}));
+  }
 }
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
 import Header from './Header';
@@ -7,19 +7,26 @@ const SurveyNew = () => <h2>SurveyNew Component</h2>
 const Dashboard = () => <h2>Dashboard Component</h2>
 const Landing = () => <h2>Landing Component</h2>
 
-function App () {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/survey/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  )
+class App extends Component {
+  componentDidMount(){
+
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/survey/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
+
 }
 
 export default App;

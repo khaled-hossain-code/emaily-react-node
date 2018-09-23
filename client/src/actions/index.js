e128a0847a2bac8394147c07f51a890bf38238fb
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-09-23 08:47:57 +0600

    Add auth reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,7 @@
 import { FETCH_USER } from './type';
 import axios from 'axios';
 
-export const fetchUser = () => {
-  return function(dispatch) {
-    axios.get('/api/current_user')
-    .then( ({data}) => dispatch({type: FETCH_USER, payload: data}));
-  }
-}
+export const fetchUser = () => async dispatch => {
+    const {data} = await axios.get('/api/current_user')
+    dispatch({type: FETCH_USER, payload: data});
+  };
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,10 @@
-export default function authReducer(state = {}, action) {
-  console.log(action);
+import {FETCH_USER} from '../actions/type';
+
+export default function authReducer(state = null, action) {
+
   switch(action.type){
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }

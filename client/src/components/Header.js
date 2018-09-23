aeb4847139c2b89aaa2bb978f1db0e431d2afe66
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-09-23 09:30:33 +0600

    Connect auth reducer with header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
-  render(){
+  renderNav() {
+    switch (this.props.auth) {
+      case null:
+        return "Unknown status";
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/auth/logout">Log out</a>
+          </li>
+        );
+    }
+  }
+  render() {
     return (
       <nav>
         <div className="nav-wrappere">
-          <a href="#" className="brand-logo">Emaily</a>
-          <ul className="right">
-            <li><a href="/auth/google">Login with Google</a></li>
-          </ul>
+          <a href="#" className="brand-logo">
+            Emaily
+          </a>
+          <ul className="right">{this.renderNav()}</ul>
         </div>
       </nav>
-    )
+    );
   }
-};
+}
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);

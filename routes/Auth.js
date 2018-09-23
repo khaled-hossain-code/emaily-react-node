343c728b6b6395de293eb91cab44539135658418
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-09-23 09:39:58 +0600

    Redirect after login

--- routes/Auth.js
+++ routes/Auth.js
@@ -6,7 +6,11 @@ module.exports = function (app) {
     req.logout();
     res.send(req.user);
   })
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => res.redirect('/surveys')
+    );
   app.get('/api/current_user', (req, res) => res.send(req.user));
 }
 
